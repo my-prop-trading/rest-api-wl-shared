786a855efb8f726a47c8ee2a7f88937c2d184694
yuriy-smplt <yuriy.p@smplt.net>
2023-09-20 17:22:45 +0300

    added configure_http_server

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod api_result_status;
 mod get_client_id;
+mod configure_rest_api_server;
 
 pub mod middlewares;
 pub use api_result_status::*;
 pub use get_client_id::*;
+pub use configure_rest_api_server::*;
