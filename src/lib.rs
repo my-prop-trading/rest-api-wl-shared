04ec91e1274a36599434de3196d361e79127e5d8
amigin <amigin@gmail.com>
2023-10-12 09:10:08 +0200

    added auth-middleware feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
 mod api_result_status;
-#[cfg(feature = "auth-middleware")]
-mod configure_rest_api_server;
 mod get_client_id;
-
 pub mod middlewares;
 pub use api_result_status::*;
+pub use get_client_id::*;
+
+#[cfg(feature = "auth-middleware")]
+mod configure_rest_api_server;
 #[cfg(feature = "auth-middleware")]
 pub use configure_rest_api_server::*;
-pub use get_client_id::*;
+
+#[cfg(not(feature = "auth-middleware"))]
+mod configure_rest_api_server_with_no_auth_middleware;
+#[cfg(not(feature = "auth-middleware"))]
+pub use configure_rest_api_server_with_no_auth_middleware::*;
