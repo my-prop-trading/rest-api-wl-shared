5d136aedaaa690734f62b12fbfe8ba708eb0d0e8
Aleksandr Belousov <aleksandr.b@simple.app>
2023-12-02 01:26:35 +0200

    https://fintechb2b.atlassian.net/browse/MVPS-814

--- src/validators.rs
+++ src/validators.rs
@@ -1,4 +1,17 @@
-use service_sdk::{my_http_server::{HttpContext, HttpFailResult}, rust_extensions::date_time::DateTimeAsMicroseconds};
+use service_sdk::{
+    my_http_server::{HttpContext, HttpFailResult},
+    rust_extensions::date_time::DateTimeAsMicroseconds,
+};
+
+pub fn validate_non_empty(_ctx: &HttpContext, value: &str) -> Result<(), HttpFailResult> {
+    if validate_non_empty_text(value) {
+        return Ok(());
+    }
+
+    Err(HttpFailResult::as_validation_error(
+        "Should not be empty".to_string(),
+    ))
+}
 
 pub fn validate_email(_ctx: &HttpContext, value: &str) -> Result<(), HttpFailResult> {
     if validate_email_text(value) {
@@ -10,15 +23,28 @@ pub fn validate_email(_ctx: &HttpContext, value: &str) -> Result<(), HttpFailRes
     ))
 }
 
+pub fn validate_email_optional(
+    _ctx: &HttpContext,
+    value: &Option<String>,
+) -> Result<(), HttpFailResult> {
+    match value {
+        Some(value) => {
+            return validate_email(_ctx, value);
+        }
+        None => Ok(()),
+    }
+}
+
 pub fn validate_password(_ctx: &HttpContext, value: &str) -> Result<(), HttpFailResult> {
     match validate_password_text(value) {
         Ok(_) => {
             if !validate_latin_letters_only(value) {
                 return Err(HttpFailResult::as_validation_error(
                     "Only latin letters are allowed".to_string(),
-                ))
+                ));
             }
-            Ok(())},
+            Ok(())
+        }
         Err(err_text) => Err(HttpFailResult::as_validation_error(err_text)),
     }
 }
@@ -38,23 +64,44 @@ pub fn validate_phone(_ctx: &HttpContext, value: &str) -> Result<(), HttpFailRes
     ))
 }
 
-pub fn validate_name(
+pub fn validate_phone_optional(
     _ctx: &HttpContext,
-    value: &str,
+    value: &Option<String>,
 ) -> Result<(), HttpFailResult> {
+    match value {
+        Some(value) => {
+            return validate_phone(_ctx, value);
+        }
+        None => Ok(()),
+    }
+}
+
+pub fn validate_name(_ctx: &HttpContext, value: &str) -> Result<(), HttpFailResult> {
     if !validate_latin_letters_only(value) {
         return Err(HttpFailResult::as_validation_error(
             "Only latin letters are allowed".to_string(),
-        ))
+        ));
     }
 
     if !validate_max(value, 32) {
         return Err(HttpFailResult::as_validation_error(
             "Max length is 32 symbols".to_string(),
-        ))
+        ));
     }
 
     return Ok(());
+}
+
+pub fn validate_name_optional(
+    _ctx: &HttpContext,
+    value: &Option<String>,
+) -> Result<(), HttpFailResult> {
+    match value {
+        Some(value) => {
+            return validate_name(_ctx, value);
+        }
+        None => Ok(()),
+    }
 }
 
 pub fn validate_date_of_birth(
@@ -71,24 +118,133 @@ pub fn validate_date_of_birth(
             if x < 18 {
                 return Err(HttpFailResult::as_validation_error(
                     "Should be older than 18".to_string(),
-                ))
+                ));
             }
 
             return Ok(());
-        },
-        service_sdk::rust_extensions::date_time::DateTimeDuration::Negative(_) => {},
-        service_sdk::rust_extensions::date_time::DateTimeDuration::Zero => {},
+        }
+        service_sdk::rust_extensions::date_time::DateTimeDuration::Negative(_) => {}
+        service_sdk::rust_extensions::date_time::DateTimeDuration::Zero => {}
     }
 
     return Err(HttpFailResult::as_validation_error(
         "Should be older than 18".to_string(),
-    ))
+    ));
+}
+
+pub fn validate_date_of_optional(
+    _ctx: &HttpContext,
+    value: &Option<DateTimeAsMicroseconds>,
+) -> Result<(), HttpFailResult> {
+    match value {
+        Some(value) => {
+            return validate_date_of_birth(_ctx, value);
+        }
+        None => Ok(()),
+    }
+}
+
+pub fn validate_date_of_birth_optional(
+    _ctx: &HttpContext,
+    value: &DateTimeAsMicroseconds,
+) -> Result<(), HttpFailResult> {
+    let now = DateTimeAsMicroseconds::now();
+    let diff = now.duration_since(*value);
+
+    match diff {
+        service_sdk::rust_extensions::date_time::DateTimeDuration::Positive(x) => {
+            // turn secunds to years
+            let x = f64::floor(x.as_secs_f64() / 60.0 / 60.0 / 24.0 / 365.0 * 100.0) / 100.0;
+            if x < 18.0 {
+                return Err(HttpFailResult::as_validation_error(
+                    "Should be older than 18".to_string(),
+                ));
+            }
+
+            return Ok(());
+        }
+        service_sdk::rust_extensions::date_time::DateTimeDuration::Negative(_) => {}
+        service_sdk::rust_extensions::date_time::DateTimeDuration::Zero => {}
+    }
+
+    return Err(HttpFailResult::as_validation_error(
+        "Should be older than 18".to_string(),
+    ));
+}
+
+/*
+ Impleent for optional
+    Address
+
+    max - 50 characters; 
+
+    not empty;
+
+    City
+
+    max - 50 characters; 
+
+    not empty;
+
+    ZIP code
+
+    max - 10 characters; 
+
+    not empty;
+
+*/
+
+pub fn validate_address_optional(_ctx: &HttpContext, value: &str) -> Result<(), HttpFailResult> {
+    if !validate_max(value, 50) {
+        return Err(HttpFailResult::as_validation_error(
+            "Max length is 50 symbols".to_string(),
+        ));
+    }
+
+    if !validate_non_empty_text(value) {
+        return Err(HttpFailResult::as_validation_error(
+            "Should not be empty".to_string(),
+        ));
+    }
+
+    return Ok(());
+}
+
+pub fn validate_city_optional(_ctx: &HttpContext, value: &str) -> Result<(), HttpFailResult> {
+    if !validate_max(value, 50) {
+        return Err(HttpFailResult::as_validation_error(
+            "Max length is 50 symbols".to_string(),
+        ));
+    }
+
+    if !validate_non_empty_text(value) {
+        return Err(HttpFailResult::as_validation_error(
+            "Should not be empty".to_string(),
+        ));
+    }
+
+    return Ok(());
+}
+
+pub fn validate_zip_code_optional(_ctx: &HttpContext, value: &str) -> Result<(), HttpFailResult> {
+    if !validate_max(value, 10) {
+        return Err(HttpFailResult::as_validation_error(
+            "Max length is 10 symbols".to_string(),
+        ));
+    }
+
+    if !validate_non_empty_text(value) {
+        return Err(HttpFailResult::as_validation_error(
+            "Should not be empty".to_string(),
+        ));
+    }
+
+    return Ok(());
 }
 
 fn validate_latin_letters_only(src: &str) -> bool {
     regex::Regex::new(r"^[a-zA-Z\-]*$").unwrap().is_match(src)
 }
-
 
 pub fn validate_non_empty_text(src: &str) -> bool {
     !src.is_empty()
@@ -113,7 +269,7 @@ const SPECIAL_SYMBOLS: [char; 13] = [
 ];
 
 fn validate_password_text(value: &str) -> Result<(), String> {
-   let mut amount_of_special_symbols = 0;
+    let mut amount_of_special_symbols = 0;
 
     let mut amount_of_spaces = 0;
 
